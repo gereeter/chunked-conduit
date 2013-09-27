3435b1cc794250c3c111726ec7c1a4bece59c47d
Jonathan S <gereeter@gmail.com>
2013-09-26 20:36:50 -0500

    Made Chunked more abstract and restructured the module to emphasize the difference between primitives and composite functions

--- src/Data/Conduit/Chunked.hs
+++ src/Data/Conduit/Chunked.hs
@@ -2,8 +2,8 @@
 
 module Data.Conduit.Chunked (
       Chunked
-    , singletonChunk
-    , headChunk
+    , makeChunk
+    , takeChunk
     , numChunks
     , concatChunks
     , mapChunks
@@ -16,7 +16,41 @@ import Data.Conduit.List (sinkNull, peek, map)
 
 import Prelude hiding (map)
 
-------------------- General Combinators ------------------------------
+---------------------- Primitive chunking ------------------------------
+
+data Chunked a = InChunk a | EndOfChunk
+
+makeChunk :: Monad m => Conduit a m (Chunked a)
+makeChunk = map InChunk >> yield EndOfChunk
+
+takeChunk :: Monad m => Conduit (Chunked a) m a
+takeChunk = do
+    mval <- await
+    case mval of
+        Nothing            -> return ()
+        Just (InChunk val) -> yield val >> takeChunk
+        Just EndOfChunk    -> return ()
+
+------------------- Composite Chunking ---------------------------------
+
+numChunks :: Monad m => Consumer (Chunked a) m Int
+numChunks = accumChunks (\x -> return (x + 1)) 0
+
+concatChunks :: Monad m => Conduit (Chunked a) m a
+concatChunks = foreverEOF takeChunk
+
+mapChunks :: Monad m => Conduit i m o -> Conduit (Chunked i) m (Chunked o)
+mapChunks p = foreverEOF (takeChunk +$= p =$= makeChunk)
+
+foldChunks :: Monad m => Consumer i m r -> Conduit (Chunked i) m r
+foldChunks p = foreverEOF ((takeChunk +$= p) >>= yield)
+
+accumChunks :: Monad m => (a -> ConduitM i o m a) -> a -> ConduitM (Chunked i) o m a
+accumChunks f = start where
+    start a = peek >>= maybe (return a) (const $ loop (f a))
+    loop consumer = (takeChunk +$= consumer) >>= start
+
+---------------------- Helper Functions --------------------------------
 
 (+$=) :: Monad m => Conduit a m b -> ConduitM b c m r -> ConduitM a c m r
 left +$= right = left =$= right' where
@@ -27,35 +61,3 @@ left +$= right = left =$= right' where
 
 foreverEOF :: Monad m => ConduitM i o m () -> ConduitM i o m ()
 foreverEOF p = peek >>= maybe (return ()) (const (p >> foreverEOF p))
-
----------------------- General chunking --------------------------------
-
-data Chunked a = InChunk a | EndOfChunk deriving (Show, Eq)
-
-singletonChunk :: Monad m => Conduit a m (Chunked a)
-singletonChunk = map InChunk >> yield EndOfChunk
-
-headChunk :: Monad m => Conduit (Chunked a) m a
-headChunk = do
-    mval <- await
-    case mval of
-        Nothing            -> return ()
-        Just (InChunk val) -> yield val >> headChunk
-        Just EndOfChunk    -> return ()
-
-numChunks :: Monad m => Consumer (Chunked a) m Int
-numChunks = accumChunks (\x -> return (x + 1)) 0
-
-concatChunks :: Monad m => Conduit (Chunked a) m a
-concatChunks = foreverEOF headChunk
-
-mapChunks :: Monad m => Conduit i m o -> Conduit (Chunked i) m (Chunked o)
-mapChunks p = foreverEOF (headChunk +$= p =$= singletonChunk)
-
-foldChunks :: Monad m => Consumer i m r -> Conduit (Chunked i) m r
-foldChunks p = foreverEOF ((headChunk +$= p) >>= yield)
-
-accumChunks :: Monad m => (a -> ConduitM i o m a) -> a -> ConduitM (Chunked i) o m a
-accumChunks f = start where
-    start a = peek >>= maybe (return a) (const $ loop (f a))
-    loop consumer = (headChunk +$= consumer) >>= start
